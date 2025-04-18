110a08776568cff3c4eb3f3fce717ffc236e46c8
Prateek <negiprateek786@gmail.com>
2025-04-18 13:13:23 +0530

    add

--- frontend/src/landing_page/signup/SignUp.jsx
+++ frontend/src/landing_page/signup/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import { useNavigate } from "react-router-dom";
-const navigate = useNavigate;
+const navigate = useNavigate();
 import { ToastContainer } from "react-toastify";
 import { handleError, handleSuccess } from "../util/error";
 
