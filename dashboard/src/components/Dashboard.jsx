0ce4020f501c0d8e2e99690284644e67f29e429e
Prateek <negiprateek786@gmail.com>
2025-04-18 13:57:24 +0530

    add

--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -15,14 +15,18 @@ const Dashboard = () => {
   const navigate = useNavigate();
   useEffect(() => {
     const token = localStorage.getItem("token");
+    const isLocal = window.location.hostname === "localhost";
+    const loginUrl = isLocal
+      ? "http://localhost:5173/login"
+      : "https://equitrade-frontend.netlify.app/login";
+
+    console.log("Redirecting to:", loginUrl); // For debug
+
     if (!token) {
-      const isLocal = window.location.hostname === "localhost";
-      const loginUrl = isLocal
-        ? "http://localhost:5173/login"
-        : "https://equitrade-frontend.netlify.app/login"; 
       window.location.href = loginUrl;
     }
   }, []);
+
   return (
     <div className="dashboard-container">
       <GeneralContextProvider>
